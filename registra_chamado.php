bf3799c54cf92afb172abaf7c62aecf41d1990c8
Matheus de Sá <matheuss_de_sa@hotmail.com>
2021-07-27 21:59:55 -0300

    teste

---
+++ registra_chamado.php
@@ -0,0 +1,22 @@
+<?php
+    //monstagem do texto
+    session_start();
+
+
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+    //abrindo o arquivo
+
+    $arquivo = fopen('arquivo.hd', 'a');
+
+    fwrite($arquivo, $texto);
+
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php');
+
+?>
---
+++ valida_login.php
@@ -0,0 +1,44 @@
+<?php
+    
+    session_start();
+
+    //Verifica se a autenticação foi realizada
+
+    $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = [1 => 'Administrativo', 2 => 'Usuário'];
+
+    //usuarios do sistema
+
+    $usuarios_app = [
+        ['id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1],
+        ['id' => 2, 'email' => 'user@teste.com.br', 'senha' => '123456', 'perfil_id' => 1],
+        ['id' => 3, 'email' => 'maria@teste.com.br', 'senha' => '123456', 'perfil_id' => 2],
+        ['id' => 4, 'email' => 'joao@teste.com.br', 'senha' => '123456', 'perfil_id' => 2],
+    ];
+
+
+    foreach($usuarios_app as $user) {
+       $user['email'];
+       $user['senha'];
+
+       if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
+       }
+    }
+
+    if ($usuario_autenticado) {
+        $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
+        header('Location: home.php');
+    } else {
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+    }
+    
+?>
